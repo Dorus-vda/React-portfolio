85891834458ac7a1e745f1776e623d45977663d9
Doruzzz <dorus.van.den.akker@coderclass.nl>
2024-02-26 01:53:38 +0100

    Hero update 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,16 @@ import { discount, robot} from '../assets'
 
 const Hero = () => (
     <section id = "home" className={` flex md:flex-row flex-col ${styles.paddingY}`}>
-
+        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+            <div className="flex flex-row items-center py-[6px] px-4 rounded-[10px] mb-2 bg-discount-gradient">
+                <p className={`${styles.paragraph} ml-2 `}>
+                <span className=' text-white'>Six year </span>
+                coderclass student</p>
+            </div>
+            <div className="flex flex-row justify-between items-center w-full">
+                <h1 className='flex-1 font-poppins ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>Dorus van den Akker</h1>
+            </div>
+        </div>
     </section>
 )
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 
-import { close, logo, menu} from '../assets'
+import { close, menu} from '../assets'
 import { navLinks } from '../constants'
 
 const Navbar = () => {
@@ -8,7 +8,7 @@ const Navbar = () => {
   
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="portfolio logo" className='w-[124px] h-[32px]'/>
+      <img src="src/assets/dlogoremovebg.png" alt="portfolio logo" className='h-[32px] animate-spin'/>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}  text-white`}>
